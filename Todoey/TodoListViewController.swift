0718881934d8fa578577b4d0e0704c0eb62ccbbd
Kristy Jackson <kristyjackson@kristys-macbook-pro.local>
2019-01-14 21:33:42 -0600

    Data persisted using user defaults method.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,13 +10,19 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    var itemArray = ["Find Mike","Buy Eggos","Destroy Demogoron"]
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
+
     }
 
-    var itemArray = ["Find Mike","Buy Eggos","Destroy Demogoron"]
-    
     //MARK - Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -39,8 +45,6 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        print (itemArray[indexPath.row])
-        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
@@ -62,6 +66,9 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the add item button on our UIAlert
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         
@@ -76,6 +83,4 @@ class TodoListViewController: UITableViewController {
         
     }
     
-    
 }
-
