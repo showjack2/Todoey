0fec20d1658a4adc85930e0082abf8693d4f76f1
Kristy Jackson <kristyjackson@kristys-macbook-pro.local>
2019-01-14 20:56:18 -0600

    Add new item forctionality complete.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -15,7 +15,7 @@ class TodoListViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    let itemArray = ["Find Mike","Buy Eggos","Destroy Demogoron"]
+    var itemArray = ["Find Mike","Buy Eggos","Destroy Demogoron"]
     
     //MARK - Tableview Datasource Methods
     
@@ -50,5 +50,32 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
+    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the add item button on our UIAlert
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+            
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
 }
 
